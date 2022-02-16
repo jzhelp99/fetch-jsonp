970e137b1f150f2354b70c6f767d286766a0a9ed
jzhelp99 <jzhelp99@gmail.com>
2022-02-16 17:11:56 +0800

    Update fetch-jsonp.js

--- src/fetch-jsonp.js
+++ src/fetch-jsonp.js
@@ -11,11 +11,13 @@ function generateCallbackFunction() {
 function clearFunction(functionName) {
   // IE8 throws an exception when you try to delete a property on window
   // http://stackoverflow.com/a/1824228/751089
+  /*
   try {
     delete window[functionName];
   } catch (e) {
     window[functionName] = undefined;
   }
+  */
 }
 
 function removeScript(scriptId) {
